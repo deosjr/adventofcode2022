e35e3ec70aad513a5aa8ba39b0dcc699bcb7f159
Sjoerd Dost <deosjr@gmail.com>
2022-12-01 20:46:49 +0100

    reverse-ish for prolog day1

--- 01/day01.pl
+++ 01/day01.pl
@@ -4,13 +4,33 @@ parse(Nums) --> integer(N), blanks, eos, {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n\n", parse([]), {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n", parse([N|Nums]).
 
+top_three(List, A, B, C) :-
+    mapsum(List, Sums),
+    sort(Sums, Sorted),
+    %permutation(Sums, Sorted),
+    %chain(Sorted, #>=),
+    reverse(Sorted, Reverse),
+    Reverse = [A,B,C|_].
+
+% maplist([X,Y]>>(sum(X, #=, Y)), List, Sums), but anon funcs like that are slow
+mapsum([], []).
+mapsum([List|T], [Sum|Sums]) :-
+    sum(List, #=, Sum),
+    mapsum(T, Sums).
+
+% the problem with running this backwards is the sorting in top_three/4
+% using permutation + checking sorted property is wayyy too slow
+test_reverse(X) :-
+    length(X, 3),
+    maplist([L]>>(length(L,N), N in 1..3, L ins 1..3), X),
+    top_three(X, 1, 2, 3),
+    maplist(label, X).
+
+% impure
 run :-
     input_stream(1, parse([])),
     findall(X, calories(X), Input),
-    maplist([X,Y]>>(sum(X, #=, Y)), Input, Sums),
-    sort(Sums, Sorted),
-    reverse(Sorted, Reverse),
-    Reverse = [A,B,C|_],
+    top_three(Input, A, B, C),
     write_part1(A),
     N #= A+B+C,
     write_part2(N).
