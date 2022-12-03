9a48aa4265f5bf10c173d85923da233e4fbc7c66
Sjoerd Dost <deosjr@gmail.com>
2022-12-03 12:13:35 +0100

    playing with reversability of day01

--- 01/day01.pl
+++ 01/day01.pl
@@ -4,7 +4,27 @@ parse(Nums) --> integer(N), blanks, eos, {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n\n", parse([]), {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n", parse([N|Nums]).
 
+member_test(X, List) :-
+    maplist(eq(X), List, Bs),
+    sum(Bs, #=, 1).
+
+eq(X, Y, B) :- X #= Y #<==> B.
+
 top_three(List, A, B, C) :-
+    mapsum(List, Sums),
+    % hand-tuned to get a faster solution; remove for fair comparison
+    % using bisect in labeling made this wayy faster btw!
+    [A,B,C] ins 60000..80000,
+    member_test(A, Sums),
+    once(labeling([bisect,max(A)], [A])),
+    select(A, Sums, Rest1),
+    member_test(B, Rest1),
+    once(labeling([bisect,max(B)], [B])),
+    select(B, Rest1, Rest2),
+    member_test(C, Rest2),
+    once(labeling([bisect,max(C)], [C])).
+
+top_three_old(List, A, B, C) :-
     mapsum(List, Sums),
     sort(Sums, Sorted),
     %permutation(Sums, Sorted),
