87bc52eb7a55033288314ffdc6d75bbae2ab238c
Sjoerd Dost <deosjr@gmail.com>
2022-12-03 12:54:02 +0100

    day01 brainwave

--- 01/day01.pl
+++ 01/day01.pl
@@ -1,36 +1,19 @@
 :- ['../lib/io.pl'].
 
+% using assert instead of parsing into list, because order doesnt matter
 parse(Nums) --> integer(N), blanks, eos, {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n\n", parse([]), {assertz(calories([N|Nums]))}.
 parse(Nums) --> integer(N), "\n", parse([N|Nums]).
 
-member_test(X, List) :-
-    maplist(eq(X), List, Bs),
-    sum(Bs, #=, 1).
-
-eq(X, Y, B) :- X #= Y #<==> B.
+select_max(X, List, Rem) :-
+    select(X, List, Rem),
+    maplist(#>(X), Rem).
 
 top_three(List, A, B, C) :-
     mapsum(List, Sums),
-    % hand-tuned to get a faster solution; remove for fair comparison
-    % using bisect in labeling made this wayy faster btw!
-    [A,B,C] ins 60000..80000,
-    member_test(A, Sums),
-    once(labeling([bisect,max(A)], [A])),
-    select(A, Sums, Rest1),
-    member_test(B, Rest1),
-    once(labeling([bisect,max(B)], [B])),
-    select(B, Rest1, Rest2),
-    member_test(C, Rest2),
-    once(labeling([bisect,max(C)], [C])).
-
-top_three_old(List, A, B, C) :-
-    mapsum(List, Sums),
-    sort(Sums, Sorted),
-    %permutation(Sums, Sorted),
-    %chain(Sorted, #>=),
-    reverse(Sorted, Reverse),
-    Reverse = [A,B,C|_].
+    select_max(A, Sums, Rem1),
+    select_max(B, Rem1, Rem2),
+    select_max(C, Rem2, _).
 
 % maplist([X,Y]>>(sum(X, #=, Y)), List, Sums), but anon funcs like that are slow
 mapsum([], []).
@@ -48,11 +31,13 @@ run :-
     write_part2(N).
 
 % BONUS: testing the reverse
-% the problem with running this backwards is the sorting in top_three/4
-% using permutation + checking sorted property is wayyy too slow
+simple_constraints(List) :-
+    length(List, N),
+    N in 1..3,
+    List ins 1..3.
+
 test_reverse(X) :-
     length(X, 3),
-    maplist([L]>>(length(L,N), N in 1..3, L ins 1..3), X),
-    top_three(X, 1, 2, 3),
+    maplist(simple_constraints, X),
+    top_three(X, 3, 2, 1),
     maplist(label, X).
-
