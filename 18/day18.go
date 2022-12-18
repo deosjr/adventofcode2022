e04d9e1ef1667002d8f6d01a9b8a3f0839394fbb
Sjoerd Dost <deosjr@gmail.com>
2022-12-18 10:24:13 +0100

    day18 proper

--- 18/day18.go
+++ 18/day18.go
@@ -2,18 +2,12 @@ package main
 
 import (
     "fmt"
-    "math"
 
 	"github.com/deosjr/adventofcode2022/lib"
-    "github.com/deosjr/Pathfinding/path"
 )
 
 type coord struct {
     x, y, z int
-}
-
-type scan struct {
-    coords map[coord]struct{}
 }
 
 func neighbours(p coord) []coord {
@@ -27,49 +21,54 @@ func neighbours(p coord) []coord {
     }
 }
 
-func (s *scan) Neighbours(n path.Node) []path.Node {
-    p := n.(coord)
-	points := []path.Node{}
-    for _, nb := range neighbours(p) {
-		if _, ok := s.coords[nb]; ok {
+func checkExternal(external, cubes map[coord]struct{}, fringe []coord, maxx,maxy,maxz int) {
+    if len(fringe) == 0 {
+        return
+    }
+    c, rest := fringe[0], fringe[1:]
+    for _, n := range neighbours(c) {
+        if n.x < -1 || n.y < -1 || n.z < -1 {
             continue
         }
-		points = append(points, nb)
-	}
-	return points
-}
-
-func (s *scan) G(pn, qn path.Node) float64 {
-    return 1.0
-}
-
-func (*scan) H(pn, qn path.Node) float64 {
-    p, q := pn.(coord), qn.(coord)
-    dx := math.Abs(float64(p.x - q.x))
-    dy := math.Abs(float64(p.x - q.x))
-    dz := math.Abs(float64(p.x - q.x))
-    return math.Sqrt(dx*dx + dy*dy + dz*dz)
+        if n.x > maxx || n.y > maxy || n.z > maxz {
+            continue
+        }
+        if _, ok := cubes[n]; ok {
+            continue
+        }
+        if _, ok := external[n]; ok {
+            continue
+        }
+        external[n] = struct{}{}
+        rest = append(rest, n)
+    }
+    checkExternal(external, cubes, rest, maxx, maxy, maxz)
 }
 
 func day18() {
-    m := map[coord]struct{}{}
-    s := &scan{m}
+    cubes := map[coord]struct{}{}
+    var maxx, maxy, maxz int
     lib.ReadFileByLine(18, func(line string) {
         var x, y, z int
         fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z)
-        m[coord{x,y,z}] = struct{}{}
+        cubes[coord{x,y,z}] = struct{}{}
+        if x > maxx { maxx = x }
+        if y > maxy { maxy = y }
+        if z > maxz { maxz = z }
     })
+
+    // guaranteed to be positive ints, so check -1 - max
+    external := map[coord]struct{}{{-1,-1,-1}:{}}
+    checkExternal(external, cubes, []coord{{0,0,0}}, maxx+1, maxy+1, maxz+1)
+
     var p1, p2 int
-    for p := range m {
+    for p := range cubes {
         for _, n := range neighbours(p) {
-            if _, ok := m[n]; ok {
+            if _, ok := cubes[n]; ok {
                 continue
             }
             p1++
-            // TODO: this is dumb overkill. instead, take bounding box once
-            // propagate external and keep in map, then just check that here
-            route, _ := path.FindRoute(s, n, coord{0,0,0})
-            if len(route) == 0 {
+            if _, ok := external[n]; !ok {
                 continue
             }
             p2++
@@ -80,6 +79,5 @@ func day18() {
 }
 
 func main() {
-    //lib.Test()
     day18()
 }
