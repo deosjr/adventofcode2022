0295ee2a9771546f12390a5b4992486ee7563930
Sjoerd Dost <deosjr@gmail.com>
2022-12-21 09:53:13 +0100

    still need to remove binary search

--- 21/day21.pl
+++ 21/day21.pl
@@ -7,24 +7,24 @@ parse --> parse_monkey(M), ": ", parse_monkey(A), " ", string_without(" ", O), "
     {atom_codes(Op, O), assertz((monkey(M, N) :- monkey(A,X), monkey(B,Y), op(Op,X,Y,N)))}.
 parse_monkey(Name) --> string_without(": \n", S), {atom_codes(Name, S)}.
 
-op(+,X,Y,Z) :-
-    Z #= X + Y.
-op(-,X,Y,Z) :-
-    Z #= X - Y.
-op(*,X,Y,Z) :-
-    Z #= X * Y.
-op(/,X,Y,Z) :-
-    Z #= X // Y.
+op(+,X,Y,Z) :- Z #= X + Y.
+op(-,X,Y,Z) :- Z #= X - Y.
+op(*,X,Y,Z) :- Z #= X * Y.
+op(/,X,Y,Z) :- Z #= X // Y.
 
 run :-
     input_stream(21, parse),
     monkey(root, Ans1),
     write_part1(Ans1),
-    % TODO: root: pgnv + wcnp
+    clause(monkey(root, _), Body),
+    Body = (monkey(A,_), monkey(B,_), _),
     retractall(monkey(root, _)),
-    assertz((monkey(root, X) :- monkey(pgnv,X), monkey(wcnp,X))),
+    assertz((monkey(root, X) :- monkey(A,X), monkey(B,X))),
     retractall(monkey(humn, _)),
     % TODO: write X instead of manual binary search...
-    assertz((monkey(humn, X) :- X in 3876027195000..3876027200000, label([X]), writeln(X))),
-    monkey(root, Ans2),
-    write_part2(Ans2).
+    X in 3876027195000..3876027200000,
+    label([X]),
+    assertz(monkey(humn, X)),
+    ( monkey(root, _) -> true ;
+      retractall(monkey(humn, _)), fail ),
+    write_part2(X).
